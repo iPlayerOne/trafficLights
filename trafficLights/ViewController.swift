1735079c1ebc1b7ed4c931278bf6b5826faa1314
Ivan Korobov <ivan.koroboff@gmail.com>
2022-06-18 17:55:22 +0200

    add lights

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var controlButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        
+        greenLight.layer.cornerRadius =
+        greenLight.frame.width / 2
     }
 
 
+    @IBAction func controlButtonAction(_ sender: UIButton) {
+    }
 }
 
