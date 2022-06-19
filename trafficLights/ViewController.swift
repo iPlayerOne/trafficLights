3b01f29cf9721ce99b149a12c91072de0d7e0feb
Ivan Korobov <ivan.koroboff@gmail.com>
2022-06-19 19:49:25 +0200

    1. Made it work for all devices. 2. Changed working logic.

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -8,27 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
     
     @IBOutlet var controlButton: UIButton!
     
+    private let isOn: CGFloat = 1
+    private let isOff: CGFloat = 0.3
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        redLight.layer.cornerRadius = redLight.frame.width / 2
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        redLight.alpha = isOff
         
-        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+        yellowLight.alpha = isOff
         
-        greenLight.layer.cornerRadius =
-        greenLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+        greenLight.alpha = isOff
+        
+        controlButton.layer.cornerRadius = 10
+        
     }
 
-
+    private var pushCounter = 0
+    
+    
     @IBAction func controlButtonAction(_ sender: UIButton) {
+        controlButton.setTitle("NEXT", for: .normal)
+        switch pushCounter {
+            case 0:
+                greenLight.alpha = isOff
+                redLight.alpha = isOn
+                pushCounter += 1
+            case 1:
+                redLight.alpha = isOff
+                yellowLight.alpha = isOn
+                pushCounter += 1
+            case 2:
+                yellowLight.alpha = isOff
+                greenLight.alpha = isOn
+                pushCounter = 0
+            default:
+                break
+        }
     }
 }
-
