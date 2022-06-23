45d614a0543a965e1f420892cb364237c2b4c6e7
Ivan Korobov <ivan.koroboff@gmail.com>
2022-06-23 19:32:58 +0200

    1. improve logic 2. improve compatibility

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -15,45 +15,50 @@ class ViewController: UIViewController {
     
     @IBOutlet var controlButton: UIButton!
     
+    
     private let isOn: CGFloat = 1
     private let isOff: CGFloat = 0.3
+    private var activeLight = ActiveLight.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        redLight.layer.cornerRadius = redLight.frame.height / 2
+    
         redLight.alpha = isOff
-        
-        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
         yellowLight.alpha = isOff
-        
-        greenLight.layer.cornerRadius = greenLight.frame.height / 2
         greenLight.alpha = isOff
         
         controlButton.layer.cornerRadius = 10
-        
     }
-
-    private var pushCounter = 0
     
+    override func viewDidLayoutSubviews() {
+        
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.height / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+    }
     
     @IBAction func controlButtonAction(_ sender: UIButton) {
         controlButton.setTitle("NEXT", for: .normal)
-        switch pushCounter {
-            case 0:
+        
+        switch activeLight {
+            case .red:
                 greenLight.alpha = isOff
                 redLight.alpha = isOn
-                pushCounter += 1
-            case 1:
+                activeLight = .yellow
+            case .yellow:
                 redLight.alpha = isOff
                 yellowLight.alpha = isOn
-                pushCounter += 1
-            case 2:
+                activeLight = .green
+            case .green:
                 yellowLight.alpha = isOff
                 greenLight.alpha = isOn
-                pushCounter = 0
-            default:
-                break
+                activeLight = .red
         }
+    }
+}
+
+extension ViewController {
+    private enum ActiveLight {
+        case red, yellow, green
     }
 }
